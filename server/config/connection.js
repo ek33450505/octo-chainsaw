c790e5e4d0c54e2551d1855c1b9ffee241b60cb5
emilyk <emily.g.kruse@gmail.com>
2022-07-02 21:20:55 -0400

    set up mysql and sequelize

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,14 @@
-const mongoose = require('mongoose');
+const Sequelize = require('sequelize');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/octo-chainsaw', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+require('dotenv').config();
 
-module.exports = mongoose.connection;
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+
+module.exports = sequelize;
