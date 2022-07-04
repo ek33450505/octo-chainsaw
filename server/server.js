a511adcaad91e0ff80624cc13c7122335df039c1
ancs214 <ancs214@gmail.com>
2022-07-04 13:43:01 -0400

    added user model

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: process.env.SECRET,
+  secret: 'process.env.SECRET',
   cookie: {},
   resave: false,
   saveUninitialized: true,
