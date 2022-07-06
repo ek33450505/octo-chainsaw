3c476f2b1f52e04f4d6d7057854e8b36da63a930
Lemon Sama <burrisc24@gmail.com>
2022-07-06 14:19:20 -0400

    seeds file

--- server/models/index.js
+++ server/models/index.js
@@ -6,12 +6,12 @@ const Category = require('./Category');
 
 //USER ASSOCIATIONS
 User.hasMany(Product, {
-    foreignKey: 'user_id' 
+    foreignKey: 'user_id'
 });
 
-User.hasMany(Rent, {
-    foreignKey: 'user_id' 
-});
+// User.hasMany(Rent, {
+//     foreignKey: 'user_id'
+// });
 
 
 //PRODUCT ASSOCIATIONS
@@ -45,4 +45,15 @@ Rent.hasOne(Product, {
 
 // Rent.belongsToMany(User,)
 
-module.exports = {User, Category, Product, Rent, Transaction};
+Rent.belongsToMany(User, {
+    through: 'users_rent',
+    as: 'users',
+    foreignKey: 'rent_id'
+})
+User.belongsToMany(Rent, {
+    through: 'users_rent',
+    as: 'rents',
+    foreignKey: 'user_id'
+})
+
+module.exports = { User, Category, Product, Rent, Transaction };
---
+++ server/seeds/index.js
@@ -0,0 +1,200 @@
+const User = require('../models/User')
+const Transaction = require('../models/Transaction')
+const Rent = require('../models/Rent');
+const Product = require('../models/Product');
+const Category = require('../models/Category')
+
+User.bulkCreate(
+    [{
+        username: 'cat',
+        email: 'cat@gmail.com',
+        password: 'password',
+        zip: '43214'
+    },
+    {
+        username: 'dog',
+        email: 'dog@gmail.com',
+        password: 'password',
+        zip: '43214'
+    },
+    {
+        username: 'cow',
+        email: 'cow@gmail.com',
+        password: 'password',
+        zip: '43214'
+    },
+    {
+        username: 'fish',
+        email: 'fish@gmail.com',
+        password: 'password',
+        zip: '43214'
+    },
+    {
+        username: 'rhino',
+        email: 'rhino@gmail.com',
+        password: 'password',
+        zip: '43214'
+    },
+    {
+        username: 'elephant',
+        email: 'elephant@gmail.com',
+        password: 'password',
+        zip: '43214'
+    }
+    ])
+
+Category.bulkCreate(
+    [
+        {
+            name: 'Outdoor Gear',
+            description: 'Gear for outdoors'
+        },
+        {
+            name: 'tools',
+            description: 'any and all tools'
+        },
+        {
+            name: 'sports',
+            description: 'any gear for sports'
+        },
+        {
+            name: 'tech',
+            description: 'get geeky'
+        },
+        {
+            name: 'motorized vehicles',
+            description: 'any thing that requires a motor to run'
+        },
+        {
+            name: 'instruments',
+            description: 'stuff that make loud noices'
+        },
+        {
+            name: 'for the kids',
+            description: 'anything for the kids'
+        },
+
+    ])
+
+Product.bulkCreate([
+    {
+        category_id: 1,
+        name: 'climbing gear',
+        description: 'All the gear you need to climb that hill',
+        img: 'put url here',
+        state: 'available',
+        price: '50.00'
+    },
+    {
+        category_id: 1,
+        name: 'snowboard',
+        description: 'a 163 burton snowboard with size 13 boots',
+        img: 'put url here',
+        state: 'available',
+        price: '50.00'
+    },
+    {
+        category_id: 2,
+        name: 'chainsaw',
+        description: 'an 18 inch ryobi 2 cycle saw that can cut through anything you need',
+        img: 'put url here',
+        state: 'available',
+        price: '30.00'
+    },
+    {
+        category_id: 2,
+        name: '30 foot extention ladder',
+        description: 'A double set of 30 foot extention ladders with window guards and a 15 foot walk board',
+        img: 'put url here',
+        state: 'available',
+        price: '100.00'
+    },
+
+    {
+        category_id: 3,
+        name: 'football helmet',
+        description: 'a schults dna helmet with the latest technology',
+        img: 'put url here',
+        state: 'available',
+        price: '50.00'
+    },
+    {
+        category_id: 3,
+        name: 'catching gear',
+        description: 'a full set of adult large catching equipment with knee savers',
+        img: 'put url here',
+        state: 'available',
+        price: '20.00'
+    },
+    {
+
+        category_id: 4,
+        name: 'drone',
+        description: 'A 1/10 scale drone flies up to 50 mph and up to 1000 feet high',
+        img: 'put url here',
+        state: 'available',
+        price: '75.00'
+    },
+    {
+        category_id: 4,
+        name: '3d printer',
+        description: 'make anything you want with this 1/20 th scale printer comes with ink',
+        img: 'put url here',
+        state: 'available',
+        price: '60.00'
+    },
+    {
+        category_id: 5,
+        name: 'Boat',
+        description: 'A wake boat full stacked',
+        img: 'put url here',
+        state: 'available',
+        price: '100.00'
+    },
+    {
+        category_id: 5,
+        name: 'quad',
+        description: 'an automatic quad that holds two people and can go 65mph',
+        img: 'put url here',
+        state: 'available',
+        price: '75.00'
+    },
+    {
+        category_id: 6,
+        name: 'drumset',
+        description: 'a drum set to bang on to drive your neighbors crazy',
+        img: 'put url here',
+        state: 'available',
+        price: '30.00'
+    }
+    ,
+    {
+        category_id: 6,
+        name: 'mayonnaise',
+        description: 'yes patrick mayonnaise is an instrument',
+        img: 'put url here',
+        state: 'available',
+        price: '1.00'
+    }
+    ,
+    {
+        category_id: 7,
+        name: 'new born clothes',
+        description: 'a weeks worth of clothes for your newborn baby ',
+        img: 'put url here',
+        state: 'available',
+        price: '5.00'
+    },
+    {
+        category_id: 7,
+        name: 'baby carriage',
+        description: 'a carriage to put your baby in',
+        img: 'put url here',
+        state: 'available',
+        price: '10.00'
+    }
+
+
+
+
+])
