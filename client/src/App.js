44d070c8a081bdb90093c4286a29aeceb514469f
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-07-07 19:07:32 -0400

    update server js

--- client/src/App.js
+++ client/src/App.js
@@ -8,26 +8,9 @@ const axios = require('axios').default;
 
 
 function App() {
-  useEffect(()=>{
-    axios({
-      method: 'post',
-      url: '/api/user',
-      data: {
-        username: "jim",
-        email: "jim@gmail.com",
-        password: "password1234",
-        zip: "43214"
-      }
-    })
-    .then(function (response) {
-      // handle success
-      console.log(response);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-  }, [])
+  // useEffect(()=>{
+    
+  // }, [])
   return (
     <div>
         <Home />
--- server/server.js
+++ server/server.js
@@ -25,16 +25,17 @@ app.use(session(sess));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/'));
-app.use(cors)
-
 // Serve up static assets - update the back-end server's code to serve up the React front-end code in production
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
   console.log("prod test " + path.join(__dirname, '../client/build'))
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/'));
+app.use(cors)
+
+
 
 app.get('*', (req, res) => {
   console.log("prod get " + path.join(__dirname, '../client/build/index.html'))
