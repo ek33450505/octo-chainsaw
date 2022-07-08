d2ee6ca2aba40be995f097871d767b0407865dab
ancs214 <ancs214@gmail.com>
2022-07-08 18:08:19 -0400

    added comments to remember the meaning of life

--- client/src/components/CategoryMenu/categorymenu.js
+++ client/src/components/CategoryMenu/categorymenu.js
@@ -6,22 +6,26 @@ import axios from 'axios';
 export default function CategoryMenu() {
     const [categories, setCategories] = useState([]);
 
+    //on page render, run fetch function
     useEffect(() => {
         fetchCategories();
     }, []);
 
+    //axios get request to fetch all categories
     const fetchCategories = async () => {
         await axios({
             method: 'get',
             url: '/api/category'
 
         })
+            //update the state with category data
             .then(function (response) {
-                console.log(response.data);
+                // console.log(response.data);
                 setCategories(response.data);
             })
     };
 
+    //map categories to cards
     return (
         <>
 
