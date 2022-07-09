c3d50e194c31dee737353025ca57fae064e4c630
ancs214 <ancs214@gmail.com>
2022-07-09 16:03:02 -0400

    small changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,10 +6,7 @@ import CategoryMenu from '../components/CategoryMenu/categorymenu.js';
 const Home = () => {
     return (
         <div>
-          
-            
             <CategoryMenu />
-           
         </div>
     )
 }
