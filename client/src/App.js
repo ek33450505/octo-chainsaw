d9b076915b722a7774cff85be4beb6a2a8e6980e
Ed Kubiak <ek33450505@gmail.com>
2022-07-10 18:08:34 -0400

    fixed another merge conflict, moving on to messaging component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import React, {useEffect} from 'react';
+import React from 'react';
 import AccountPage from './pages/AccountPage'
-const axios = require('axios').default;
+// const axios = require('axios').default;
 
 
 function App() {
