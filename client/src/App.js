79f33600228e092571de8b302f225aac5a764fb0
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-07-10 19:05:21 -0400

    get latest from develop

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import CategoriesPage from './pages/CategoriesPage';
 import React, {useEffect} from 'react';
-const axios = require('axios').default;
+const axios = require('axios');
+
 
 // send a POST request
 
@@ -13,6 +16,7 @@ function App() {
   return (
     <div>
         <LoginPage />
+        <CategoriesPage/>
     </div>
   );
 }
