a9381729c7730713117b36e0c710e2edcbaf6472
ancs214 <ancs214@gmail.com>
2022-07-11 09:54:22 -0400

    isolated inner text of category selected button on button click

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,40 +1,48 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from 'axios';
 
 
 
 export default function ProductList() {
-  const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
-  //on page render, run fetch function
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+    //on page render, run fetch function
+    useEffect(() => {
+        fetchProducts();
+    }, []);
 
-  //axios get request to fetch all products
-  const fetchProducts = async () => {
-    await axios({
-      method: 'get',
-      url: '/api/product'
+    //axios get request to fetch all products
+    const fetchProducts = async () => {
+        await axios({
+            method: 'get',
+            url: '/api/product'
 
-    })
-      //update the state with product data
-      .then(function (response) {
-        // console.log(response.data);
-        setProducts(response.data);
-      })
-  };
+        })
+            //update the state with product data
+            .then(function (response) {
+                // console.log(response.data);
+                setProducts(response.data);
+            })
+    };
 
-  //map products to page
-  return (
-    <div>
-      { products.map(element => {
-        return (
-          <h4>{ element.name }</h4>
-        )
-      })}   
-    </div>
-  )
+    //map products to page
+    return (
+        <div>
+            {products.map(element => {
+                return (
+                    <>
+                        <Link to='/api/product' key={element.id}>
+                            <h4 key={element.id}>{element.name}</h4>
+                        </Link>
+                        <p>{ element.description }</p>
+                        <p>{ element.state }</p>
+                        <p>{ element.price }</p>
+                    </>
+                )
+            })}
+        </div>
+    )
 }
 
 
