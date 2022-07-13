3359c1f9069be4d72743c7277144c1a7a1781a8d
michael <shinlin09@gmail.com>
2022-07-12 20:06:57 -0400

    styles

---
+++ client/src/pages/ContactPages.js
@@ -0,0 +1,18 @@
+import Contact from '../components/Contact/contact';
+import Nav from '../components/Nav/nav.js';
+import Footer from '../components/Footer/footer.js';
+import Hero from '../components/Hero/hero.js';
+
+
+const ContactPage = () => {
+    return (
+        <div>
+            <Nav />
+            <Hero />
+            <Contact />
+            <Footer />
+        </div>
+    )
+}
+
+export default ContactPage;
