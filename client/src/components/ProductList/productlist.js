4b6ad93583247eb3630264f346ce93e50243c274
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-07-27 00:40:15 -0400

    refactor the product list component and fix the duplicate product name

--- client/src/components/ProductList/productlist.js
+++ client/src/components/ProductList/productlist.js
@@ -7,8 +7,10 @@ import axios from 'axios';
 
 export default function ProductList({ currentCategory }) {
     const [products, setProducts] = useState([]);
-    const [currentProduct, setCurrentProduct] = useState("");
+    const [currentProductId, setCurrentProductId] = useState("");
     const [buttonPopup, setButtonPopup] = useState(false);
+    const allProducts = products.filter(p => currentCategory === p.category.name);
+
     //on page render, run fetch function
 
     useEffect(() => {
@@ -29,88 +31,98 @@ export default function ProductList({ currentCategory }) {
             })
     };
 
-    //map products to cards
-    return (
-        <>
-            <div>
-                <section id="portfolio" className="portfolio">
-                    <div className="container">
+    //return a single product with a popup
+    const renderProduct = (product) => {
+        if (currentProductId === product.id) {
+            return (
+                <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+                    <img src='https://images.freeimages.com/images/large-previews/c03/colour-math-function-1170167.jpg' className="img-fluid" alt=""></img>
+                    <h3>{product.name}</h3>
+                    <h3>{product.description}</h3>
+                    <h3>{product.price}</h3>
+                    <p>Status: {product.state}</p>
 
-                        <div className="section-title text-center" data-aos="fade-left">
-                            <h2>Products</h2>
-                        </div>
-                        <div className="items-container">
-                            <div className="row portfolio-container flex-item" data-aos="fade-up" data-aos-delay="200">
+                    <div className="col-md-3 form-group">
+                        <p><Calendar currentState={product.state} /></p>
+                        <button type='button' className='rentit-button2' onClick={() => window.location.reload()}> Rent </button>
+                    </div>
+                </Popup>
+            )
+        }
+    }
 
-                                {currentCategory ? (
-                                    products.filter(p => currentCategory === p.category.name).map(product => (
+    //render all products from a category
+    const renderProducts = () => {
 
-                                        <div className="col-lg-4 col-md-6 portfolio-item filter-app">
-                                            <div className="portfolio-wrap">
-                                                <img src='https://images.freeimages.com/images/large-previews/c03/colour-math-function-1170167.jpg' className="img-fluid" alt=""></img>
-                                                {/* console.log(product.image_url) */}
-                                                {/* <img src=("../../assets/img/categories/portfolio-1.png") className="img-fluid" alt=""></img> */}
-                                                {/* <img src={require(`../../../../server/${element.image_url}`)} className="img-fluid" alt=""></img> */}
-                                                <div className="portfolio-info">
-                                                    <h4>{product.name}</h4>
-                                                    <div className="portfolio-links">
-                                                        {/* <a href="#" data-gallery="portfolioGallery" className="portfolio-lightbox" title="App 1"><i className="bx bx-plus"></i></a> */}
-                                                        <p>{product.description}</p>
-                                                        <p>Rental Price: ${product.price}</p>
-                                                        <p>Status: {product.state}</p>
-                                                        <p><Calendar currentState={product.state}/></p>
-                                                        <div className='row right-button'>
-                                                            <div className="col-md-3 form-group">
-                                                                {/* <button type='button' className='rentit-button1' onClick={()=> setCurrentProduct(product.id)}><Link to='/'>Rent</Link>   </button> */}
-                                                                <button type='button' className='rentit-button1' onClick={()=> window.location.reload()}> Rent </button>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    ))
-                                ) : (
-                                    products.map(product => (
-                                        <div>
-                                            <div>
-                                                {/* <h4 onClick={() => setCurrentProduct(product.id)}>{product.name}</h4> */}
-                                                <h4 onClick={() => setCurrentProduct(product.id)}>{product.name}</h4>
-                                            </div>
-
-                                            {currentProduct === product.id ? (
-                                                <div className='product-details product-selected'>
-                                                    <h4 onClick={() => setButtonPopup(true)}>{product.name}</h4>
-
-                                                    <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-                                                        <img src='https://images.freeimages.com/images/large-previews/c03/colour-math-function-1170167.jpg' className="img-fluid" alt=""></img>
-                                                        <h3>{product.name}</h3>
-                                                        <h3>{product.description}</h3>
-                                                        <h3>{product.price}</h3>
-                                                        <p>Status: {product.state}</p>
-                                                        
-                                                        <div className="col-md-3 form-group">
-                                                        <p><Calendar currentState={product.state} /></p>
-                                                                <button type='button' className='rentit-button2' onClick={()=> window.location.reload()}> Rent </button>
-                                                            </div>
-                                                    </Popup>
-                                                </div>
-                                            ) : null}
-                                        </div>
-                                    ))
-                                )
-                                }
-
-
-
+        return allProducts.map(product => {
+            return (
+                <div className="col-lg-4 col-md-6 portfolio-item filter-app">
+                    <div className="portfolio-wrap">
+                        {/* <img src={require(`../../../../server/${element.image_url}`)} className="img-fluid" alt=""></img> */}
+                        <img src='https://images.freeimages.com/images/large-previews/c03/colour-math-function-1170167.jpg' className="img-fluid" alt=""></img>
+                        {/* console.log(product.image_url) */}
+                        {/* <img src=("../../assets/img/categories/portfolio-1.png") className="img-fluid" alt=""></img> */}
+                        {/* <img src={require(`../../../../server/${element.image_url}`)} className="img-fluid" alt=""></img> */}
+                        <div className="portfolio-info">
+                            <h4>{product.name}</h4>
+                            <div className="portfolio-links">
+                                {/* <a href="#" data-gallery="portfolioGallery" className="portfolio-lightbox" title="App 1"><i className="bx bx-plus"></i></a> */}
+                                <p>{product.description}</p>
+                                <p>Rental Price: ${product.price}</p>
+                                <p>Status: {product.state}</p>
+                                <p><Calendar currentState={product.state} /></p>
+                                <div className='row right-button'>
+                                    <div className="col-md-3 form-group">
+                                        {/* <button type='button' className='rentit-button1' onClick={()=> setCurrentProduct(product.id)}><Link to='/'>Rent</Link>   </button> */}
+                                        <button type='button' className='rentit-button1' onClick={() => window.location.reload()}> Rent </button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-
                     </div>
+                </div>
+            )
+        })
+    }
 
-                </section>
-            </div>
-        </>
+    //set product id and popup the product
+    const setProduct = (product) => {
+        setCurrentProductId(product.id);
+        setButtonPopup(true);
+    }
+
+    // display the content on the page
+    const renderContent = () => {
+        if (currentCategory != null && currentCategory !== "") {
+            return renderProducts();
+        } else {
+            return (
+                products.map(product => (
+                    <div>
+                        <h4 onClick={() => setProduct(product)}>{product.name}</h4>
+                        {renderProduct(product)}
+                    </div>
+                ))
+            )
+        }
+    }
+
+    //map products to cards
+    return (
+        <div>
+            <section id="portfolio" className="portfolio">
+                <div className="container">
+                    <div className="section-title text-center" data-aos="fade-left">
+                        <h2>Products</h2>
+                    </div>
+                    <div className="items-container">
+                        <div className="row portfolio-container flex-item" data-aos="fade-up" data-aos-delay="200">
+                            {renderContent()}
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
 
